5ccf2b97c074133f1a78d4dd23edc0f57b18426b
benny <benny.nottonson01@student.csulb.edu>
2023-06-24 10:26:28 -0700

    optimize production (WIP)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+const webpack = require("webpack");
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
@@ -7,4 +10,20 @@ module.exports = withBundleAnalyzer({
   images: {
     domains: ["i.scdn.co", "mosaic.scdn.co"],
   },
+  webpack: (config, {webpack}) => {
+    config.module.rules.push({
+      resolve: {
+        alias: {
+          three$: path.resolve("./build/three-exports.js"),
+        },
+      },
+    }),
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^three$/,
+        contextRegExp: /three/,
+      }),
+    )
+    return config; 
+  }
 });
