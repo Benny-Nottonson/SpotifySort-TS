a15e05e429e49655fbd719db6dee9e4a08b7cef5
benny <benny.nottonson01@student.csulb.edu>
2023-07-03 19:21:56 -0700

    webpack optimizations

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
-
+const webpack = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
+
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
@@ -14,14 +17,43 @@ module.exports = withBundleAnalyzer({
     locales: ["en"],
     defaultLocale: "en",
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      resolve: {
-        alias: {
-          three$: path.resolve("./build/three-exports.js"),
+  webpack: (config, { isServer, dev }) => {
+    if (!dev && !isServer) {
+      config.optimization.splitChunks = {
+        chunks: 'async',
+        minSize: 20000,
+        minRemainingSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        enforceSizeThreshold: 50000,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
         },
-      },
-    });
+      };
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+          },
+          extractComments: false,
+        }),
+      ];
+      config.plugins.push(new CompressionPlugin());
+      config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
+    }
+    config.resolve.alias["three$"] = path.resolve("./build/three-exports.js");
     return config;
   },
 });
