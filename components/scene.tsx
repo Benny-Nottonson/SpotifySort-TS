6e984cd0127a6efb5f53f2da776d44210859a3db
benny <benny.nottonson01@student.csulb.edu>
2023-07-07 12:38:50 -0700

    bg fix

--- components/scene.tsx
+++ components/scene.tsx
@@ -144,7 +144,7 @@ function AnimatedCircle({
 export default function Scene({ ...props }) {
   return (
     <>
-      <color attach="background" args={["#000414"]} />
+      <color attach="background" args={[0, 4, 20]} />
       <group {...props} dispose={null}>
         <AnimatedCircle
           name="Small Top Right"
